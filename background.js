75b1626fff8cea6cda7a2c2ebfbc74801daf1869
Mat <Matgenius04@gmail.com>
2019-02-26 15:36:39 -0500

    add more basic stuff

--- background.js
+++ background.js
@@ -2,4 +2,11 @@ chrome.runtime.onInstalled.addListener(()=>{
     chrome.storage.sync.set({quizlet_sets: {}}, ()=>{
 
     })
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+        chrome.declarativeContent.onPageChanged.addRules([{
+          conditions: [new chrome.declarativeContent.PageStateMatcher({})
+          ],
+              actions: [new chrome.declarativeContent.ShowPageAction()]
+        }]);
+      });
 })
--- content.js
+++ content.js
@@ -1,1 +1,16 @@
-console.log("I'm on quizlet");
+let all = document.getElementsByClassName("TermText");
+let terms = [];
+let definitions = [];
+window.addEventListener('load',()=>{
+    for (x>all.length;;) {
+    if (x%2) {
+        terms.push(document.getElementsByClassName("TermText")[x])
+    } else {
+        definitions.push(document.getElementsByClassName("TermText")[x])
+    }
+    if (x%10){
+        console.log(terms[1] + definitions[1])
+    }
+}
+
+})
