e6103cc151682e75eeb419350f0ad08992cbf7a6
Mat <Matgenius04@gmail.com>
2019-02-27 21:40:10 -0500

    Forgot to get rid of a localstorage thing

--- background.js
+++ background.js
@@ -1,7 +1,4 @@
 chrome.runtime.onInstalled.addListener(()=>{
-    chrome.storage.sync.set({quizlet_sets: {}}, ()=>{
-
-    })
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
         chrome.declarativeContent.onPageChanged.addRules([{
           conditions: [new chrome.declarativeContent.PageStateMatcher({})
