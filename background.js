df277107b36fa1ee716bb6d953b2b41c58119db8
Mat <Matgenius04@gmail.com>
2019-03-04 14:45:35 -0500

    starting to add flashcard stuff

--- background.js
+++ background.js
@@ -1,29 +1,30 @@
-//If popup button pressed, open popup
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({})],
-      actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-  });
+chrome.runtime.onInstalled.addListener(()=>{
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+        chrome.declarativeContent.onPageChanged.addRules([{
+          conditions: [new chrome.declarativeContent.PageStateMatcher({})
+          ],
+              actions: [new chrome.declarativeContent.ShowPageAction()]
+        }]);
+      });
 });
-
-//INDEXEDDB
 altEval = x => {
-  let request = indexedDB.open('sets', 1);
+  console.log(x);
+  let request = indexedDB.open('sets',1);
   request.onerror = event => {
     alert("Database error: " + event.target.errorCode);
   }
   request.onsuccess = event => {
     let db = request.result;
-    let name = x.name;
-    let set = x.set;
-    // db.transaction('x','readwrite').objectStore('x').get(0).onsuccess = e => {console.log(e.target.result)}
-    db.transaction('x', "readwrite").objectStore('x').put({
-      name: name,
-      set: set,
-      key: 0
-    }).onsuccess = e => {};
+    let transaction = db.transaction('x','readwrite');
+    transaction.oncomplete = e => {
+      alert('opened transaction')
+    }
+  let objectStore = transaction.objectStore('x');
+  let objectStoreRequest = objectStore.get('0');
+  objectStoreRequest.onsuccess = e => {
+    objectStoreRequest.add(x);
+    alert('hope')
+  }
+    db.transaction('x',"readwrite").objectStore('x')
   }
 }
-//INDEXEDDB END
--- popup/popup.js
+++ popup/popup.js
@@ -54,7 +54,71 @@ window.addEventListener('load', () => {
     }
   }
   //INDEXEDDB END
+   function flipString(aString) {
+    var last = aString.length - 1;
+    var result = new Array(aString.length)
+    for (var i = last; i >= 0; --i) {
+     var c = aString.charAt(i)
+     var r = flipTable[c]
+     result[last - i] = r != undefined ? r : c
+    }
+    return result.join('')
+   }
+   var flipTable = {
+   a : '\u0250',
+   b : 'q',
+   c : '\u0254', 
+   d : 'p',
+   e : '\u01DD',
+   f : '\u025F', 
+   g : '\u0183',
+   h : '\u0265',
+   i : '\u0131', 
+   j : '\u027E',
+   k : '\u029E',
+   //l : '\u0283',
+   m : '\u026F',
+   n : 'u',
+   r : '\u0279',
+   t : '\u0287',
+   v : '\u028C',
+   w : '\u028D',
+   y : '\u028E',
+   '.' : '\u02D9',
+   '[' : ']',
+   '(' : ')',
+   '{' : '}',
+   '?' : '\u00BF',
+   '!' : '\u00A1',
+   "\'" : ',',
+   '<' : '>',
+   '_' : '\u203E',
+   ';' : '\u061B',
+   '\u203F' : '\u2040',
+   '\u2045' : '\u2046',
+   '\u2234' : '\u2235',
+   '\r' : '\n' 
+   }
+   for (i in flipTable) {
+     flipTable[flipTable[i]] = i
+   }
 
+  let flashcard = document.createElement('div');
+  let flashCardShow = string => {
+    flashcard.innerHTML = `<h3 style="margin:auto;width:50%;text-align: center; padding: 10px; position:relative;">${string}<h3>`;
+    flashcard.style = "transform: rotate3d(0);height:100px;width:200px;";
+    document.getElementById('flashcard-wrapper').appendChild(flashcard);
+  }
+  let flashCardFlip = string => {
+    // flippedString = flipString(string.toLowerCase())
+    flashcard.style = "transform: rotate3d(1,0, 0, 0.50turn);transform: rotate(180deg); transition-duration: 3s;height:100px;width:200px;";
+    setTimeout(()=>{flashcard.innerHTML = `<h3 style="margin:auto;width:50%;text-align: center; padding: 10px; position:relative;">${string}<h3>`;},900)
+  }
+
+flashCardShow('test')
+setTimeout(()=>{flashCardFlip('testing')},3000)
+
+  //add set on quizlet
   let addSet = function () {
     chrome.tabs.query({
       active: true,
